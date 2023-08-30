409815224299b52bf4123a691b29e9c5f5771545
BrandonOh <brandonoh2938@gmail.com>
2023-08-30 18:33:11 -0400

    updated model for database and made register item page.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Item model
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    inv = models.IntegerField()
+    sale = models.DecimalField(max_digits=2,decimal_places=2)
+    desc = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8 ,decimal_places=2)
+    food_type = models.CharField(max_length=50)
+    food_image = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    def __str__(self):
+        return(f"{self.name}")
--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register_item', views.register_item,name='register_item')
 ]
